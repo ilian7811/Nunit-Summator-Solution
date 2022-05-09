6b7cb465b50b4ad26ec1061235965cddffe59af6
ilian7811 <ilian7811@gmail.com>
2022-05-09 12:31:34 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if(result == 20)
+            {
+                Console.WriteLine("TEST PASS");
+            }else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
